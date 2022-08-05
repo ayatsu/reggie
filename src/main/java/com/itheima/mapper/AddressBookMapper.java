7597e6110089131a485bfedc78be49a8126789e0
ayatsu <1525466429@qq.com>
2022-08-05 22:36:06 +0800

    test

--- src/main/java/com/itheima/mapper/AddressBookMapper.java
+++ src/main/java/com/itheima/mapper/AddressBookMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.entity.AddressBook;
 import org.apache.ibatis.annotations.Mapper;
 
+//
 @Mapper
 public interface AddressBookMapper extends BaseMapper<AddressBook> {
 
